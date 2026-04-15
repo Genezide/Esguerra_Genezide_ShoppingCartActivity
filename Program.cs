0aa4e02a161e6070a130709d7ae7a068351d73ea
Genezide <genezideesguerra@gmail.com>
2026-04-15 19:53:21 +0800

    created cart item class

--- Program.cs
+++ Program.cs
@@ -18,21 +18,16 @@ class Product
     }
 }
 
+class CartItem
+{
+    public Product Product;
+    public int Quantity;
+}
+
 class Program
 {
     static void Main()
     {
-        Product[] products = new Product[3];
-
-        products[0] = new Product { Id = 1, Name = "Laptop", Price = 30000, RemainingStock = 5 };
-        products[1] = new Product { Id = 2, Name = "Mouse", Price = 500, RemainingStock = 10 };
-        products[2] = new Product { Id = 3, Name = "Keyboard", Price = 1200, RemainingStock = 7 };
-
-        Console.WriteLine("STORE MENU");
-
-        foreach (Product p in products)
-        {
-            p.DisplayProduct();
-        }
+        Console.WriteLine("Shopping Cart System");
     }
 }
