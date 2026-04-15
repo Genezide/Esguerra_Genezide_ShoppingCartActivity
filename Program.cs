72afaa94a6b26eab8f37f7ad739a1a00b3b1d81d
Genezide <genezideesguerra@gmail.com>
2026-04-15 17:40:16 +0800

    added product menu display

--- Program.cs
+++ Program.cs
@@ -6,12 +6,28 @@ class Product
     public string Name;
     public double Price;
     public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - Price: {Price} - Stock: {RemainingStock}");
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Shopping Cart System");
+        Product[] products = new Product[3];
+
+        products[0] = new Product { Id = 1, Name = "Laptop", Price = 30000, RemainingStock = 5 };
+        products[1] = new Product { Id = 2, Name = "Mouse", Price = 500, RemainingStock = 10 };
+        products[2] = new Product { Id = 3, Name = "Keyboard", Price = 1200, RemainingStock = 7 };
+
+        Console.WriteLine("STORE MENU");
+
+        foreach (Product p in products)
+        {
+            p.DisplayProduct();
+        }
     }
 }
