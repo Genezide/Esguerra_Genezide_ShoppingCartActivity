946ee6d907b6ffd9e9f1310e91650ed9c022dfa5
Genezide <genezideesguerra@gmail.com>
2026-04-30 18:59:11 +0800

    Added Product class for inventory system

---
+++ Product.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class Product
+{
+    public int Id;
+    public string Name;
+    public double Price;
+    public int RemainingStock;
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine($"{Id}. {Name} - Price: {Price} - Stock: {RemainingStock}");
+    }
+
+    public bool HasEnoughStock(int quantity)
+    {
+        return quantity <= RemainingStock;
+    }
+
+    public void DeductStock(int quantity)
+    {
+        RemainingStock -= quantity;
+    }
+}
